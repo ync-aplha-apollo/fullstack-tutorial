2469f90b8be2f00317b07510c8bce737e61582a2
yongnan <ync@fastmail.com>
2022-04-10 10:24:37 +0800

    react-app

---
+++ my/client/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from "../styles/global";
+
+const Pages: React.FC = () => {
+  return (
+    <>
+        <GlobalStyle />
+      pages
+    </>
+  );
+}
+
+export default Pages;
---
+++ my/client/src/styles/global.ts
@@ -0,0 +1,65 @@
+import {createGlobalStyle} from "styled-components"
+
+// export default createGlobalStyle`
+//     *{
+//         margin: 0;
+//         padding: 0;
+//         outline:0;
+//         box-sizing:border-box;
+//         font-family: 'Open Sans', sans-serif; 
+//     }
+//     #root{
+//         margin:0 auto;
+//     }
+    
+// `
+export const unit = 8;
+export const colors = {
+  primary: '#220a82',
+  secondary: '#14cbc4',
+  accent: '#e535ab',
+  background: '#f7f8fa',
+  grey: '#d8d9e0',
+  text: '#343c5a',
+  textSecondary: '#747790'
+};
+
+export default createGlobalStyle`
+  [['html', 'body']]: {
+    height: '100%',
+  },
+  body: {
+    margin: 0,
+    padding: 0,
+    fontFamily: "'Source Sans Pro', sans-serif",
+    backgroundColor: colors.background,
+    color: colors.text,
+  },
+  '#root': {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100%',
+  },
+  '*': {
+    boxSizing: 'border-box',
+  },
+  [['h1', 'h2', 'h3', 'h4', 'h5', 'h6']]: {
+    margin: 0,
+    fontWeight: 600,
+  },
+  h1: {
+    fontSize: 48,
+    lineHeight: 1,
+  },
+  h2: {
+    fontSize: 40,
+  },
+  h3: {
+    fontSize: 36,
+  },
+  h5: {
+    fontSize: 16,
+    textTransform: 'uppercase',
+    letterSpacing: 4,
+  }
+`;
